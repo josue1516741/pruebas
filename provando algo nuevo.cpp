f1f899b7ae2413a138dfdfa497ac5e7ed28c6d4a
Josue Enamorado <enamoradojr11@gmail.com>
2022-03-06 13:39:42 -0600

    primera

---
+++ provando algo nuevo.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+#include <iostream>
+#include<conio.h>
+char pass[10];
+using namespace std;
+int correcto;
+int intento;
+int op;
+const char contrasena[]="12345"; /*12345 es la contraseña que se debe introducir*/
+
+
+int main(void){
+system("color b0");
+    intento=1;
+    correcto=0;
+    printf("Introduzca la contrasena: \n"); gets(pass); printf("\n");
+    if (strcmp(pass,contrasena)==0) correcto=1;
+    while ((correcto==0)&&(intento<3))
+    {
+        intento++;
+        printf("Contrasena incorrecta. %i intento: ", intento); gets(pass); printf("\n");
+        if (strcmp(pass,contrasena)==0) correcto=1;
+    }
+    
+       	
+    if (correcto==0) printf("Se han excedido el numero de intentos.");
+   
+    else
+     system("cls");
+        
+        	cout<<"*********************************"<<endl;
+		cout<<"*********************************"<<endl;
+            printf("Bienvenido a la base de datos. \n");
+          cout<<"*********************************"<<endl;  
+		cout<<"*********************************"<<endl;
+            /*Aqui se puede poner lo que se quiera para que solo se tenga acceso en el caso de tener la contraseña*/
+            cout<< "Ingrese la opcion que desea utilizar"<<endl;
+            cin>>op;
+            system("cls");
+            
+            switch (op)
+            case 1:
+		  {
+           int numero, pares, impares;
+
+    printf( "\n   Introduzca un n%cmero entero (0=Fin): ", 163 );
+    scanf( "%d", &numero );
+
+    pares = 0;
+    impares = 0;
+
+    while ( numero != 0 )
+    {
+        if ( numero % 2 == 0 )
+            pares += numero;
+        else
+            impares += numero;
+
+        printf( "\n   Introduzca un n%cmero entero (0=Fin): ", 163 );
+        scanf( "%d", &numero );
+    }
+
+    printf( "\n   La suma de los pares es: %d", pares );
+    printf( "\n\n   La suma de los impares es: %d", impares );
+
+    getch(); /* Pausa */
+          
+}
+        
+    return 0;
+}

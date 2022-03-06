fcb5a2a70a974de29c8a4522cbb943e01a5171ba
Josue Enamorado <enamoradojr11@gmail.com>
2022-03-06 13:55:17 -0600

    modificar

--- provando algo nuevo.cpp
+++ provando algo nuevo.cpp
@@ -40,32 +40,6 @@ system("color b0");
             cin>>op;
             system("cls");
             
-            switch (op)
-            case 1:
-		  {
-           int numero, pares, impares;
-
-    printf( "\n   Introduzca un n%cmero entero (0=Fin): ", 163 );
-    scanf( "%d", &numero );
-
-    pares = 0;
-    impares = 0;
-
-    while ( numero != 0 )
-    {
-        if ( numero % 2 == 0 )
-            pares += numero;
-        else
-            impares += numero;
-
-        printf( "\n   Introduzca un n%cmero entero (0=Fin): ", 163 );
-        scanf( "%d", &numero );
-    }
-
-    printf( "\n   La suma de los pares es: %d", pares );
-    printf( "\n\n   La suma de los impares es: %d", impares );
-
-    getch(); /* Pausa */
           
 }
         
